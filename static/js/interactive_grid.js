993c1179afc562f1ff9304fed522eb912502ad69
CoderN-P <76001641+CoolCoderN@users.noreply.github.com>
2022-02-09 16:22:57 -0800

    added search bar

--- static/js/interactive_grid.js
+++ static/js/interactive_grid.js
@@ -1,44 +1,11 @@
-const rangeInput = document.querySelector('input[type = "range"]');
-const imageList = document.querySelector(".image-list");
-const searchInput = document.querySelector('input[type="search"]');
-const btns = document.querySelectorAll(".view-options button");
-const photosCounter = document.querySelector(".toolbar .counter span");
-const imageListItems = document.querySelectorAll(".image-list li");
-const captions = document.querySelectorAll(".image-list figcaption p:first-child");
-const myArray = [];
+const searchInput = document.querySelector('input');
+myArray = [];
 let counter = 1;
-const active = "active";
-const listView = "list-view";
-const gridView = "grid-view";
-const dNone = "d-none";
-
-// SET VIEW
-for (const btn of btns) {
-  btn.addEventListener("click", function() {
-    const parent = this.parentElement;
-    document.querySelector(".view-options .active").classList.remove(active);
-    parent.classList.add(active);
-    this.disabled = true;
-    document.querySelector('.view-options [class^="show-"]:not(.active) button').disabled = false;
-
-    if (parent.classList.contains("show-list")) {
-      parent.previousElementSibling.previousElementSibling.classList.add(dNone);
-      imageList.classList.remove(gridView);
-      imageList.classList.add(listView);
-    } else {
-      parent.previousElementSibling.classList.remove(dNone);
-      imageList.classList.remove(listView);
-      imageList.classList.add(gridView);
-    }
-  });
-}
-
-// SET THUMBNAIL VIEW - CHANGE CSS VARIABLE
-rangeInput.addEventListener("input", function() {
-  document.documentElement.style.setProperty("--minRangeValue",`${this.value}px`);
-});
-
-// SEARCH FUNCTIONALITY
+const dNone = 'd-none';
+const imageListItems = document.querySelectorAll("#country-div");
+console.log(imageListItems);
+const captions = document.querySelectorAll("#country-name p");
+console.log(captions);
 for (const caption of captions) {
   myArray.push({
     id: counter++,
@@ -49,15 +16,19 @@ for (const caption of captions) {
 searchInput.addEventListener("keyup", keyupHandler);
 
 function keyupHandler() {
+  console.log('hi');
   for (const item of imageListItems) {
     item.classList.add(dNone);
   }
   const text = this.value;
   const filteredArray = myArray.filter(el => el.text.includes(text));
+  console.log(filteredArray);
   if (filteredArray.length > 0) {
     for (const el of filteredArray) {
-      document.querySelector(`.image-list li:nth-child(${el.id})`).classList.remove(dNone);
+      console.log(el.id);
+      let a = document.querySelector(`#country-list a:nth-child(${el.id})`).children[0];
+
+      a.classList.remove(dNone);
     }
   }
-  photosCounter.textContent = filteredArray.length;
 }
